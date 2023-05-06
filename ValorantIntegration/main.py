7aeeeb9ff745898aca147f316e23acdf4fab7f3c
itsKannapi <112032889+itsKannapi@users.noreply.github.com>
2023-05-05 19:51:39 -0500

    started integration code

---
+++ ValorantIntegration/main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+player = "Kannapi"
+Valorant_Ranks_List = ["Iron", "Bronze", "Silver", "Gold", "Platinum", "Diamond", "Ascendant", "Immortal", "Radiant"]
+Valorant_RankNum = [1, 2, 3]
+
